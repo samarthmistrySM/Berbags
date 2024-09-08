541a928056b66433ecd5ce1d23e195f1f7a92ffa
Samarth Mistry <g33ky.sam@gmail.com>
2024-09-08 16:56:25 +0530

    updated token value

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,10 +2,8 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
-  const token = req.cookies.token;
-  console.log(authHeader);
-  console.log(":");
-  console.log(token);
+  const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) {
     return res.status(401).send('Please Login first to perform such operation!');
   }
